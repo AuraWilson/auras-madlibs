a28f621d3b126187e472bec50a00cd843c7c2f77
AuraWilson <wilsonaura2006@gmail.com>
2024-07-31 21:02:01 +0000

    put a short description of your changes here

---
+++ script.js
@@ -0,0 +1,30 @@
+let food = "teriyaki"
+let drink = "smoothie"
+let like = "i like"
+let and = "and"
+let wallet = "60"
+let did = "did you know"
+let myNumber = "59+1"
+let eq = "equals"
+let designer = "moschino"
+let car = "porsche"
+let i = "it is"
+let ii = "it is not"
+let o = "outside"
+//boolean - true or false
+let sunny = true
+let rain = false
+let myName = "aura"
+let userName = prompt("enter your name") //save result of the user entering their name into the prompt window
+console.log('Hello, ' + myName + " " + like + " " + food + " " + and + " " + drink)
+console.log('Hello, ' + myName + " " + did + " " + myNumber + " " + eq + " " + wallet)
+console.log('Hello, ' + myName + " " + i + " " + sunny + " " + o + " " + and + " " + ii + " " + rain + " " + o)
+//create null and undefined
+
+//mix data types
+
+//string methods
+
+//link up html file and create prompt and save value
+
+//access the DOM
